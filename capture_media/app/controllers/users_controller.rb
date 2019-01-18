cb718766361a4bced876ff67c44de53dfb0e5dbc
Breyanna Vass <breyannavass@gmail.com>
2019-01-18 18:11:18 -0500

    Notes for later

--- capture_media/app/controllers/users_controller.rb
+++ capture_media/app/controllers/users_controller.rb
@@ -12,6 +12,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
+      #test to see if this will redirect to home_path
       redirect_to user_path(@user)
     else
       redirect_to :new
